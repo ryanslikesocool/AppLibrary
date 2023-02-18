f788ae18e4ff4a423eaaee9b661d9f7305c5e28a
Ryan Boyer <ryanjboyer@gmail.com>
2023-02-17 20:55:41 -0700

    Initial Commit

---
+++ AppLibrary/AppLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLibraryApp.swift
+//  AppLibrary
+//
+//  Created by RyanBoyer on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct AppLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
