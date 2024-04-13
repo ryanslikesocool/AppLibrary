e22f33a134fcc7c26d2c65f015d4d200080d44f1
Ryan Boyer <ryanjboyer@gmail.com>
2024-04-13 16:32:58 -0600

    Support reduce transparency option for search field

--- AppLibraryModules/Sources/AppLibraryBrowser/Extensions/AppSettings.Display+Extension.swift
+++ AppLibraryModules/Sources/AppLibraryBrowser/Extensions/AppSettings.Display+Extension.swift
@@ -1,9 +1,13 @@
 import AppKit
+import SwiftUI
 import AppLibraryStorage
 
 extension AppSettings.Display {
 	var launcherBackgroundMaterial: NSVisualEffectView.Material {
 		reduceTransparency ? .windowBackground : .underWindowBackground
+	}
+	var searchBackgroundMaterial: AnyShapeStyle {
+		reduceTransparency ? AnyShapeStyle(.background) : AnyShapeStyle( .thinMaterial)
 	}
 
 	var willReduceMotion: Bool { NSWorkspace.shared.accessibilityDisplayShouldReduceMotion || reduceMotion }
--- AppLibraryModules/Sources/AppLibraryBrowser/Views/SearchField.swift
+++ AppLibraryModules/Sources/AppLibraryBrowser/Views/SearchField.swift
@@ -1,11 +1,14 @@
 import SwiftUI
+import AppLibraryStorage
 
 struct SearchField: View {
+	@ObservedObject private var appSettings: AppSettings = .shared
 	@ObservedObject private var browserModel: BrowserModel = .shared
 	@FocusState private var isFocused: Bool
 
 	private var shadowA: Shadow { isFocused ? Self.focusedShadowA : Self.unfocusedShadowA }
 	private var shadowB: Shadow { isFocused ? Self.focusedShadowB : Self.unfocusedShadowB }
+	private var stroke: Stroke { isFocused ? Self.focusedStroke : Self.unfocusedStroke }
 
 	var body: some View {
 		TextField(text: $browserModel.searchQuery, prompt: Text("􀊫 App Library"), label: EmptyView.init)
@@ -15,8 +18,8 @@ struct SearchField: View {
 			.textFieldStyle(.plain)
 
 			.padding(Self.innerPadding)
-			.background(.separator, in: containerShape.stroke(lineWidth: 1))
-			.background(.ultraThinMaterial, in: containerShape)
+			.overlay(stroke.style, in: containerShape.stroke(lineWidth: stroke.width))
+			.background(appSettings.display.searchBackgroundMaterial, in: containerShape)
 			.padding(Self.outerPadding)
 			.fixedSize(horizontal: false, vertical: true)
 			.compositingGroup()
@@ -40,12 +43,16 @@ struct SearchField: View {
 
 private extension SearchField {
 	typealias Shadow = (opacity: Double, radius: Double, y: Double)
+	typealias Stroke = (style: AnyShapeStyle, width: Double)
 
 	static let unfocusedShadowA: Shadow = (0.1, 1, 0.5)
 	static let unfocusedShadowB: Shadow = (0.05, 2, 1)
 
-	static let focusedShadowA: Shadow = (0.2, 2, 1)
-	static let focusedShadowB: Shadow = (0.1, 4, 2)
+	static let focusedShadowA: Shadow = (0.25, 4, 2)
+	static let focusedShadowB: Shadow = (0.125, 8, 4)
+
+	static let unfocusedStroke: Stroke = (AnyShapeStyle(.separator), 1)
+	static let focusedStroke: Stroke = (AnyShapeStyle(Color.accentColor), 2)
 
 	static let innerPadding: CGFloat = 8
 	static let outerPadding: CGFloat = 8
